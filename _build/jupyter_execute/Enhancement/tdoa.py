e9e33a208bb7faabb580ab54169f95c0860c4837
Tom Bäckström <tom.backstrom@aalto.fi>
2022-04-30 15:28:45 +0300

    tdoa complete

--- _build/jupyter_execute/Enhancement/tdoa.py
+++ _build/jupyter_execute/Enhancement/tdoa.py
@@ -6,6 +6,39 @@
 # In [multi-channel speech enhancement](Multi-channel_speech_enhancement.md), a regularly appearing task is to estimate the time-delay between channels or equivalently, the angle at which a wavefront arrives to an array of microphones. By knowing the time-delay or angle of arrival, we can use beamforming to isolate sounds from that particular direction. 
 # A frequently used method for time-delay estimation is the generalized cross-correlation (GCC) method and especially its PHAT-weighted variant known as GCC-PHAT {cite:p}`knapp1976gcc,azaria1984time,byoungho2010gccphat`.
 # 
+# ## Generalized cross-correlation
+# 
+# The cross-spectrum of two spectra $X_{1,k,t}$ and $X_{2,k,t}$ is
+# $$
+# C_{k,t} = X_{1,k,t}^* X_{2,k,t},
+# $$
+# where $k$ and $t$ are the frequency and time indices. The spectra are of form $X_{h,k}=a_{h,k} e^{i\frac{2\pi kn_h}N}$, where $n_h$ is the time-offset and $N$ is the length of the analysis window. We thus have
+# $$
+# C_k 
+# = a_{1,k} e^{-i\frac{2\pi kn_1}N} a_{2,k} e^{i\frac{2\pi kn_2}N}
+# .
+# $$
+# If the time-difference between channels is $\tau=n_2-n_1$, then
+# $$
+# C_k 
+# = a_{1,k}a_{2,k} e^{-i\frac{2\pi kn_1}N+i\frac{2\pi k(\delta+n_1)}N}
+# = a_{1,k}a_{2,k} e^{i\frac{2\pi k\tau}N}
+# .
+# $$
+# 
+# 
+# It can be weighted with a variety of approaches such as
+# $$
+# C_k' = \frac{X_{1,k}^* X_{2,k}}{|X_{1,k}^* X_{2,k}|} = e^{i\frac{2\pi k\delta}N}
+# $$
+# to obtain the generalized cross-spectrum. The generalized cross-correlation is the inverse Fourier transform of the generalized cross-spectrum
+# $$
+# r_k' = {\mathcal F}^{-1}\{C_k'\} = \delta_\tau,
+# $$
+# where $\delta_k$ is the [Dirac-delta function](https://en.wikipedia.org/wiki/Dirac_delta_function). In other words, the generalized cross-covariance has a single peak whose position indicates the time-delay $\tau$ between the two channels.
+# 
+# 
+# 
 # <!--
 # Suppose we have two channels, for which we have analyzed the [short-time Fourier transforms (STFT)](stft), $X_{1,k,t}$, and $X_{2,k,t}$, where $k$ is the frequency bin and $t$ the frame index. The frequency components correspond to complex values of the form
 # 
@@ -74,7 +107,7 @@ plt.plot(crosscorrelation[0:100],label='Cross-correlation')
 plt.plot([delay_samples,delay_samples],[-.1,.6],'r--',label='True delay location')
 plt.legend()
 plt.xlabel('Sample $k$')
-plt.ylabel('Cross-correlation $r_k$')
+plt.ylabel("Generalized cross-correlation $r_k'$")
 
 plt.tight_layout()
 plt.show()
