91bacfd3a0f2f8be6bdfc8a314b305761fd5f670
Tom Bäckström <tom.backstrom@aalto.fi>
2023-01-12 14:28:44 +0200

    interpretation of spectrograms

--- Representations/helper_functions.py
+++ Representations/helper_functions.py
@@ -69,3 +69,56 @@ def zcr(data,fs,window_length_ms=30,window_step_ms=20):
         
     return y
     
+    
+
+
+def freq2mel(f): return 2595*np.log10(1 + (f/700))
+def mel2freq(m): return 700*(10**(m/2595) - 1)
+
+
+def melfilterbank(speclen, maxfreq, melbands = 20):
+    maxmel = freq2mel(maxfreq)
+    mel_idx = np.array(np.arange(.5,melbands,1)/melbands)*maxmel
+    freq_idx = mel2freq(mel_idx)
+
+    melfilterbank = np.zeros((speclen,melbands))
+    freqvec = np.arange(0,speclen,1)*maxfreq/speclen
+    for k in range(melbands-2):    
+        if k>0:
+            upslope = (freqvec-freq_idx[k])/(freq_idx[k+1]-freq_idx[k])
+        else:
+            upslope = 1 + 0*freqvec
+        if k<melbands-3:
+            downslope = 1 - (freqvec-freq_idx[k+1])/(freq_idx[k+2]-freq_idx[k+1])
+        else:
+            downslope = 1 + 0*freqvec
+        triangle = np.max([0*freqvec,np.min([upslope,downslope],axis=0)],axis=0)
+        melfilterbank[:,k] = triangle
+
+    melreconstruct = np.matmul(np.diag(np.sum(melfilterbank**2+1e-12,axis=0)**-1),np.transpose(melfilterbank))
+                  
+    return melfilterbank, melreconstruct
+
+
+def linearfilterbank(speclen, maxfreq_Hz, bandwidth_Hz=500):
+    bandstep_Hz = bandwidth_Hz/2
+    bands = int(maxfreq_Hz/bandstep_Hz)+1 
+    filterbank = np.zeros((speclen,bands))
+    freqvec = np.arange(0,speclen,1)*maxfreq_Hz/speclen
+    freq_idx = np.arange(-bandstep_Hz/2,maxfreq_Hz+bandstep_Hz/2,bandstep_Hz)
+    for k in range(bands-1):
+        if k>0:
+            upslope = (freqvec-freq_idx[k])/(freq_idx[k+1]-freq_idx[k])
+        else:
+            upslope = 1 + 0*freqvec
+        if k<bands-2:
+            downslope = 1 - (freqvec-freq_idx[k+1])/(freq_idx[k+2]-freq_idx[k+1])
+        else:
+            downslope = 1 + 0*freqvec
+        triangle = np.max([0*freqvec,np.min([upslope,downslope],axis=0)],axis=0)
+        filterbank[:,k] = triangle
+                          
+    reconstruct = np.matmul(np.diag(np.sum(filterbank**2+1e-12,axis=0)**-1),np.transpose(filterbank))
+                  
+    return filterbank, reconstruct
+    
