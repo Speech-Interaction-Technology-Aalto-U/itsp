2f05630a48539bb765bbbd035602256ad9579700
Tom Bäckström <tom.backstrom@aalto.fi>
2024-09-13 09:11:49 +0300

    Fix image axis

--- generate_requirements.sh
+++ generate_requirements.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-conda install pipreqs nbconvert
+#conda install pipreqs nbconvert
 jupyter nbconvert --output-dir="./reqs" --to script *.ipynb */*.ipynb */*.py
 cd reqs
 rm requirements.txt
 pipreqs
 cd ..
-mv reqs/requirements.txt .
+#mv reqs/requirements.txt .
